f8a1ffe2c4908fa8f6f0996421f2f63b69274c68
Emma Stotz <stotz.emma@gmail.com>
2019-09-25 23:10:23 -0400

    Working through the logic, timer/answerCheck functions updated.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,95 @@
 $(document).ready(function(){
-// GLOBAL VARIABLES ========================================
+  // GLOBAL VARIABLES ========================================
+  var timer = 120;
+  var timeId;
+  var correctAnswers = 0;
+  var incorrectAnswers = 0;
+  var unanswered = 0;
 
+  // FUNCTIONS ===============================================
+  //Render Questions
+  let gameManager = {
+    setGameStart: function() {
+      this.setQuestion();
+      this.setAnswers();
+    },
+    setQuestion: function (){
 
-// FUNCTIONS ===============================================
+    },
+    setAnswers: function (){
 
+    }
+  }
+  
+  //Check Answers
+  function checkAnswers (){
+    for (var q = 1; q <= 10; q++){
+      for (var a = 1; a <= 3; a++){
+        var answerValue = document.getElementById("inlineRadio" + [a]).value;
+        //console.log(answerValue);
+        if (answerValue === "correct"){
+          correctAnswers++;
+          //console.log(correctAnswers);
+        }
+        if (answerValue === "incorrect"){
+          incorrectAnswers++;
+        }
+        else {
+          unanswered++;
+        }
+      }
+    }
+  };
+
+  //Timer 
+  $("#timer").text("0:00")
+  $(".question-container").hide();
+  $(".timer-container").hide();
+  $("#done").hide();
+
+  $("#play").on("click",function(){
+    function startTimer(){
+      clearInterval(timeId);
+      timeId = setInterval(decrement, 1000);
+    };
+    function decrement(){
+      timer--;
+      var converted = timeConverter(timer);
+      $("#timer").text(converted);
+      if (timer === 0) {
+        stopTimer();
+      }
+    };
+    function timeConverter(t) {
+      var minutes = Math.floor(t / 60);
+      var seconds = t - (minutes * 60);
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+      if (minutes === 0) {
+        minutes = "0";
+      }
+      return minutes + ":" + seconds;
+    }
+    $(".question-container").show();
+    $(".timer-container").show();
+    $("#play").hide();
+    $("#done").show();
+    startTimer();
+    
+    if (timer === 0){
+      clearInterval(timeId);
+      $(".timer-container").hide();
+      $(".question-container").hide();
+      checkAnswers();
+    }
+    $("#done").on("click",function(){
+      clearInterval(timeId);
+      $(".timer-container").hide();
+      $(".question-container").hide();
+      $("#done").hide();
+      checkAnswers();
+    })
+  });
 
 });
---
+++ assets/javascript/questions.js
@@ -0,0 +1,104 @@
+let questions;
+
+var questions = [
+  {
+    // Question 1
+    question: "1. Monica briefly dates billionaire Pete Becker. Which country does he take her for their first date?",
+    answers: {
+      a: "Italy",
+      b: "France",
+      c: "Greece",
+    },
+    correctAnswer: "a"
+  },
+  {
+    // Question 2
+    question: "2. Rachel was popular in high school. Her prom date Chip ditched her for which girl at school?",
+    answers: {
+      a: "Sally Roberts",
+      b: "Amy Welsh",
+      c: "Valerie Thompson",
+    },
+    correctAnswer: "b"
+  },
+  {
+    // Question 3
+    question: "3. Chandler is a unique first name, but Bing is something different! Chandler says that his last name is Gaelic for what?",
+    answers: {
+      a: "Huzzah! The team has scored!",
+      b: "The turkey's done.",
+      c: "Thou hast received a telegram.",
+    },
+    correctAnswer: "b"
+  },
+  {
+    // Question 4
+    question: "4. How much of Joey’s heritage is Portuguese?",
+    answers: {
+      a: "1/4",
+      b: "1/8",
+      c: "1/16",
+    },
+    correctAnswer: "c"
+  },
+  {
+    // Question 5
+    question: "5. What fake name does Phoebe use when she wants to remain anonymous?",
+    answers: {
+      a: "Regina Phalange",
+      b: "Bonnie Riddle",
+      c: "Anastasia Beaverhausen",
+    },
+    correctAnswer: "a"
+  },
+  {
+    // Question 6
+    question: "6. Monica and Chandler met on Thanksgiving in 1987. She pursued her career as a chef because Chandler complimented her on which dish?",
+    answers: {
+      a: "Mac & Cheese",
+      b: "Stuffing",
+      c: "Green Bean Casserole",
+    },
+    correctAnswer: "a"
+  },
+  {
+    // Question 7
+    question: "7. When Ross and Rachel were 'on a break,' Ross slept with Chloe. Where does she work?",
+    answers: {
+      a: "Domino's",
+      b: "Xerox",
+      c: "Microsoft",
+    },
+    correctAnswer: "b"
+  },
+  {
+    // Question 8
+    question: "8. What’s Joey’s penguin’s name?",
+    answers: {
+      a: "Huggsy",
+      b: "Waddle",
+      c: "Snowflake",
+    },
+    correctAnswer: "a"
+  },
+  {
+    // Question 9
+    question: "9. Janice gets married twice. What’s was her first husband’s name?",
+    answers: {
+      a: "Chandler Bing",
+      b: "Gary Litman",
+      c: "Rob Baileystock",
+    },
+    correctAnswer: "b"
+  },
+  {
+    // Question 10
+    question: "10. What’s the name of the 1950s-themed diner where Monica worked as a waitress?",
+    answers: {
+      a: "Marilyn & Audrey",
+      b: "Central Perk",
+      c: "Moondance Diner",
+    },
+    correctAnswer: "c"
+  },
+]
